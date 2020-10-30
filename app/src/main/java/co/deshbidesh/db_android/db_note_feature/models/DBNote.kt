c7beafb527638e73fcc6535b95df836506a5307b
Resham gurung <reshamgurung01@gmail.com>
2020-10-30 20:30:36 +0000

    Parcelable added to note object, Main nav updated with notes fragments

--- app/src/main/java/co/deshbidesh/db_android/db_note_feature/models/DBNote.kt
+++ app/src/main/java/co/deshbidesh/db_android/db_note_feature/models/DBNote.kt
@@ -1,12 +1,14 @@
 package co.deshbidesh.db_android.db_note_feature.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import co.deshbidesh.db_android.db_database.database.DatabaseConverter.DBNoteLanguageConverter
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
-
+@Parcelize
 @Entity(tableName = "db_notes")
 @TypeConverters(DBNoteLanguageConverter::class)
 data class DBNote (
@@ -25,4 +27,4 @@ data class DBNote (
     val updatedDate:    Date?,
 
     val languageType:   DBNoteLanguage
-)
+): Parcelable
